eaee3912b0c25345104168a9fbe02179a2bd1cf9
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-07-03 08:28:53 +0100

    Improve responsivness

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,61 @@ export default function Home() {
   const boxRef = useRef<HTMLDivElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
+  const createGrid = (engine: Matter.Engine) => {
+    const distance = 32; // distance between dots
+    const dotRadius = 3; // radius of each dot
+    const margin = 64 + dotRadius / 2; // margin around the canvas
+
+    const gridWidth = window.innerWidth - margin * 2; // width of the grid
+    const gridHeight = window.innerHeight - margin * 2; // height of the grid
+
+    let numDotsX = Math.floor(gridWidth / distance);
+    let numDotsY = Math.floor(gridHeight / distance);
+
+    // Create fake grid to test position of the dots
+    // const fakeGrid = Matter.Bodies.rectangle(
+    //   gridWidth / 2 + margin,
+    //   gridHeight / 2 + margin,
+    //   gridWidth,
+    //   gridHeight,
+    //   {
+    //     isStatic: true,
+    //     render: {
+    //       opacity: 0.6,
+    //       fillStyle: "blue",
+    //     },
+    //   }
+    // );
+
+    // Matter.World.add(engine.world, fakeGrid);
+
+    // Create background grid of dots
+    for (let y = 0; y < numDotsY + 1; y++) {
+      const numDotsXModulo = y % 2 ? numDotsX : numDotsX + 1;
+
+      for (let x = 0; x < numDotsXModulo; x++) {
+        let xPos = x === 0 ? margin : x * (gridWidth / numDotsX) + margin;
+        if (y % 2) xPos += distance / 2;
+
+        const yPos = y === 0 ? margin : y * (gridHeight / numDotsY) + margin;
+
+        const dot = Matter.Bodies.circle(
+          Math.ceil(xPos),
+          Math.ceil(yPos),
+          dotRadius,
+          {
+            isStatic: true,
+            render: {
+              fillStyle: "#E2E2E2",
+            },
+          }
+        );
+
+        Matter.World.add(engine.world, dot);
+      }
+    }
+  };
+
   useLayoutEffect(() => {
     let engine = Matter.Engine.create({});
 
@@ -20,10 +75,6 @@ export default function Home() {
         wireframes: false,
       },
     });
-
-    const distance = 32; // distance between dots
-    const dotRadius = 3; // radius of each dot
-    const margin = 64 + dotRadius / 2; // margin around the canvas
 
     let balls: Matter.Body[] = [];
     const ballRadius = 8; // radius of each ball
@@ -53,74 +104,11 @@ export default function Home() {
       render.canvas.width = window.innerWidth;
       render.canvas.height = window.innerHeight;
 
-      // Removing everything and then adding it again doesn't feel like a great way to handle resize
-      // Find a better way to remove while resizing
-      // for (let i = 0; i < engine.world.bodies.length; i++) {
-      //   const body = engine.world.bodies[i];
-      //   console.log(body.label);
-      //   if (body.label === "Circle Body") {
-      //     Matter.World.remove(engine.world, body);
-      //   }
-      // }
+      // Clear the world
+      Matter.World.clear(engine.world, false);
 
-      const gridWidth = window.innerWidth - margin * 2; // width of the grid
-      const gridHeight = window.innerHeight - margin * 2; // height of the grid
-
-      let numDotsX = Math.floor(gridWidth / distance);
-      let numDotsY = Math.floor(gridHeight / distance);
-
-      // Create fake grid to test position of the dots
-      // const fakeGrid = Matter.Bodies.rectangle(
-      //   gridWidth / 2 + margin,
-      //   gridHeight / 2 + margin,
-      //   gridWidth,
-      //   gridHeight,
-      //   {
-      //     isStatic: true,
-      //     render: {
-      //       opacity: 0.6,
-      //       fillStyle: "blue",
-      //     },
-      //   }
-      // );
-
-      // Matter.World.add(engine.world, fakeGrid);
-
-      // Create background grid of dots
-      for (let y = 0; y < numDotsY + 1; y++) {
-        const numDotsXModulo = y % 2 ? numDotsX : numDotsX + 1;
-
-        for (let x = 0; x < numDotsXModulo; x++) {
-          let xPos = x === 0 ? margin : x * (gridWidth / numDotsX) + margin;
-          if (y % 2) xPos += distance / 2;
-
-          const yPos = y === 0 ? margin : y * (gridHeight / numDotsY) + margin;
-
-          const dot = Matter.Bodies.circle(
-            Math.ceil(xPos),
-            Math.ceil(yPos),
-            dotRadius,
-            {
-              isStatic: true,
-              render: {
-                fillStyle: "#E2E2E2",
-              },
-            }
-          );
-
-          Matter.World.add(engine.world, dot);
-        }
-      }
-
-      // Not sure about this but it seems to work
-      for (let i = 0; i < balls.length; i++) {
-        const ball = balls[i];
-        const newX =
-          ball.position.x * (window.innerWidth / render.options.width);
-        const newY =
-          ball.position.y * (window.innerHeight / render.options.height);
-        Matter.Body.setPosition(ball, { x: newX, y: newY });
-      }
+      // Create grid
+      createGrid(engine);
     };
 
     // Annoyingly we need to call this at the beginning as well as on resize
